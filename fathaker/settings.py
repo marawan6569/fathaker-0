a5005971e8bc110799cba32737d9e7cbf29c7af0
marawan6569 <marawan6569@gmail.com>
2023-02-28 23:35:52 +0200

    add CSRF_TRUSTED_ORIGINS

--- fathaker/settings.py
+++ fathaker/settings.py
@@ -131,3 +131,5 @@ MEDIA_ROOT = 'media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = os.getenv('TRUSTEDORIGINS').split(';')
